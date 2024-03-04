3ca51bafe1f3deb7b2ea70c4b3b14aade0e7a42f
nikithasrii <98526622+nikithasrii@users.noreply.github.com>
2024-03-04 10:59:04 +0530

    Time: 45 ms (7.49%) | Memory: 16.5 MB (47.97%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        command=command.replace("(al)","al")
+        command=command.replace("()","o")
+        return command
+            
+
+
+        
